3b3538154a7cc12306a192c0a01aa6befed50022
KAP <KAP@DESKTOP-DNB9NIU>
2022-09-22 09:48:23 +0300

    Добавьте файлы проекта.

---
+++ Alarm/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.m = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // m
+            // 
+            this.m.BackColor = System.Drawing.Color.Black;
+            this.m.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.m.Dock = System.Windows.Forms.DockStyle.Left;
+            this.m.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.m.ForeColor = System.Drawing.Color.Gray;
+            this.m.Location = new System.Drawing.Point(4, 4);
+            this.m.Mask = "90:00";
+            this.m.Name = "m";
+            this.m.Size = new System.Drawing.Size(79, 34);
+            this.m.TabIndex = 0;
+            this.m.Text = "1000";
+            this.m.ValidatingType = typeof(System.DateTime);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Gray;
+            this.button1.Location = new System.Drawing.Point(208, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(83, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(253, -6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(21, 19);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(267, 44);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.m);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(4);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox m;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Alarm/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alarm
+{
+    public partial class Form1 : Form
+    {
+        private DateTime dtstart;
+        private SoundPlayer play = null;
+        public Form1()
+        {
+            InitializeComponent();
+            play = new SoundPlayer(new FileStream("Alarm09.wav", FileMode.Open));
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Location = new Point();
+            TopMost = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dtstart = DateTime.Now;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var t =dtstart.TimeOfDay - DateTime.Now.TimeOfDay + new TimeSpan(0, int.Parse(m.Text.Split(':')[0]), int.Parse(m.Text.Split(':')[1]));
+            label1.Text = $"{t.Minutes:00}:{t.Seconds:00}";
+            if (t.Minutes == 0 && t.Seconds == 0)
+            {
+                timer1.Enabled = false;
+                play.Play();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
